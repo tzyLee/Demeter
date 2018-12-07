552d3ea662512a7423ba49bed9e6b0eb82fab2b3
ByronHsu <byronhsu1230@gmail.com>
2018-12-08 03:15:16 +0800

    finish quality eval

--- line_bot.py
+++ line_bot.py
@@ -23,6 +23,7 @@ from subprocess import run
 import pyimgur
 from test import inference
 import cv2
+from template import score_bubble, help_prompt
 
 app = Flask(__name__)
 CLIENT_ID = '068f642d85cf4ba'
@@ -32,76 +33,6 @@ line_bot_api = LineBotApi(
 handler = WebhookHandler('da2d6f9e4ea684b0141df09cfeb0bb89')
 
 images_collection = {}
-
-bubble = FlexSendMessage(alt_text="help", contents=BubbleContainer(
-    direction='ltr',
-    hero=ImageComponent(
-        url='https://i.pinimg.com/originals/e6/4a/ab/e64aabf950b63781ce7ae2d05ce026dd.png',
-        size='full',
-        aspect_ratio='20:13',
-        aspect_mode='cover'
-    ),
-    body=BoxComponent(
-        layout='vertical',
-        contents=[
-            TextComponent(text='品質偵測', weight='bold', size='xl'),
-            BoxComponent(
-                layout='baseline',
-                margin='md',
-                contents=[
-                    IconComponent(
-                        size='md', url='https://i.imgur.com/qGdfV69.png'),
-                    IconComponent(
-                        size='md', url='https://i.imgur.com/qGdfV69.png'),
-                    IconComponent(
-                        size='md', url='https://i.imgur.com/qGdfV69.png'),
-                    IconComponent(
-                        size='md', url='https://i.imgur.com/qGdfV69.png'),
-                    IconComponent(
-                        size='md', url='https://i.imgur.com/3g5n4Wv.png'),
-                    TextComponent(text='4.0', size='sm',
-                                  color='#999999', margin='md', flex=0)
-                ]
-            )
-        ],
-    ),
-    footer=BoxComponent(
-        layout='vertical',
-        spacing='sm',
-        contents=[
-            SpacerComponent(size='sm'),
-            ButtonComponent(
-                style='link',
-                height='sm',
-                action=URIAction(label='TODO', uri='tel:000000'),
-            ),
-            SeparatorComponent()
-        ]
-    ),
-))
-
-help_prompt = TemplateSendMessage(
-    alt_text='幫助選單',
-    template=CarouselTemplate(
-        columns=[
-            CarouselColumn(
-                thumbnail_image_url='https://i.imgur.com/WAqchIp.jpg',
-                title='幫助選單',
-                text='拍照或上傳照片即可為您進行評分',
-                actions=[
-                    CameraAction(
-                        label='拍照',
-                        text='camera'
-                    ),
-                    CameraRollAction(
-                        label='上傳',
-                        text='camera roll'
-                    )
-                ]
-            )
-        ]
-    )
-)
 
 @app.route('/')
 def hello_world():
@@ -130,18 +61,18 @@ def handle_message(event):
     reply = None
     if message == '上傳評分':
         reply = help_prompt
-    elif message == '測試':
-        reply = bubble
-    elif len(message) > 1 and message[0] == '-':
-        run(message[1:].split(' '))
-        return
-    elif len(message) > 1 and message[0] == '+':
-        img_name = message[1:]
-        link = images_collection[img_name]
-        reply = ImageSendMessage(
-                original_content_url=link, preview_image_url=link)
-    elif message.isdigit():
-        reply = StickerSendMessage(package_id='1', sticker_id=message)
+    elif message == '價格查詢':
+        reply = URIAction()
+    #elif len(message) > 1 and message[0] == '-':
+    #    run(message[1:].split(' '))
+    #    return
+    #elif len(message) > 1 and message[0] == '+':
+    #    img_name = message[1:]
+    #    link = images_collection[img_name]
+    #    reply = ImageSendMessage(
+    #            original_content_url=link, preview_image_url=link)
+    #elif message.isdigit():
+    #    reply = StickerSendMessage(package_id='1', sticker_id=message)
     else:
         reply = TextSendMessage(text=message)
     line_bot_api.reply_message(event.reply_token, reply)
@@ -150,7 +81,6 @@ def handle_message(event):
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
     image = line_bot_api.get_message_content(event.message.id)
-    # print(image)
     try:
         '''
         fileName = strftime('%Y-%m-%d_%H:%M:%S')
@@ -161,20 +91,13 @@ def handle_image_message(event):
         img_raw = b''.join(image.iter_content())
         img = Image.open(BytesIO(img_raw))
         img_arr = np.array(img)
-        print('img_arr', img_arr)
         output = inference(img_arr)
-        print('output', output)
+        output = int(output)
+        reply = score_bubble(output)
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=f'Class {output}'))
-        
-        #uploaded = im.upload_image(fileName, title=fileName)
-        #images_collection[fileName] = uploaded.link
-        #line_bot_api.reply_message(event.reply_token, TextSendMessage(
-        #    text=f'Successfully uploaded to imgur, link is {uploaded.link}'))
+            event.reply_token, reply)
     except ValueError:
         print('err')
-    #    line_bot_api.reply_message(event.reply_token, TextSendMessage(
-    #        text='Error occurred during uploading to imgur'))
 
 if __name__ == "__main__":
     app.run(ssl_context=('/etc/nginx/ssl/cert.pem', '/etc/nginx/ssl/cert.key'), port = 8081)
---
+++ template.py
@@ -0,0 +1,95 @@
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    ImageMessage, ImageSendMessage,
+    TextSendMessage,
+    StickerSendMessage,
+    TemplateSendMessage,
+    CarouselTemplate,
+    CarouselColumn,
+    CameraAction,
+    CameraRollAction,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent, URIAction,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent, SeparatorComponent
+)
+
+def score_bubble(CLASS):
+   title = None
+   description = None
+   image_url = None
+   stars = None
+   
+   if CLASS == 0:
+      title = '良級'
+      description = '品質次於優級品，但有商品價值者。'
+      stars = 1   
+   elif CLASS == 1:
+      title = '優級'
+      description = '同一品種，成熟尚適度，果形尚完整，色澤良好，無嚴重病蟲害及其他傷害。'
+      stars = 3
+   elif CLASS == 2:
+      title = '特級'
+      description = '同一品種，成熟適度，果形完整，色澤優良，無病蟲害及其他傷害。'
+      stars = 5
+   stars_contents = []
+
+   for i in range(stars):
+      stars_contents.append(IconComponent(size='md', url='https://i.imgur.com/qGdfV69.png')) # gold star
+   while len(stars_contents) < 5:
+      stars_contents.append(IconComponent(size='md', url='https://i.imgur.com/YYw2wxe.png')) # black star
+
+   stars_contents.append(TextComponent(text=str(stars), size='sm', color='#999999', margin='md', flex=0))
+
+   bubble=FlexSendMessage(alt_text="help", contents=BubbleContainer(
+      direction='ltr',
+      header=BoxComponent(
+         layout='vertical',
+         contents=[
+            TextComponent(text='品質分析', weight='bold', size='xl')     
+         ]
+      ),
+      hero=ImageComponent(
+         url='https://i.imgur.com/UmRYpmh.jpg',
+         size='full',
+         aspect_ratio='20:13',
+         aspect_mode='cover'
+      ),
+      body=BoxComponent(
+         layout='vertical',
+         contents=[
+             TextComponent(text=title, size='lg'),
+             BoxComponent(
+                 layout='baseline',
+                 margin='md',
+                 contents=stars_contents
+             ),
+             TextComponent(text=description, wrap=True, size='md')
+         ]
+      )
+   ))
+   return bubble
+
+help_prompt = TemplateSendMessage(
+   alt_text='幫助選單',
+   template=CarouselTemplate(
+      columns=[
+         CarouselColumn(
+            thumbnail_image_url='https://i.imgur.com/WAqchIp.jpg',
+            title='幫助選單',
+            text='拍照或上傳照片即可為您進行評分',
+            actions=[
+               CameraAction(
+                   label='拍照',
+                   text='camera'
+               ),
+               CameraRollAction(
+                   label='上傳',
+                   text='camera roll'
+               )
+            ]
+         )
+      ]
+   )
+)
+
+
